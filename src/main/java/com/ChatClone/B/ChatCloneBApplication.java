b223a8dd4fe1d77d63e90252181b01e2069ae952
Ugongjae <turtle0191@naver.com>
2021-01-11 22:48:50 +0900

    first commit

---
+++ src/main/java/com/ChatClone/B/ChatCloneBApplication.java
@@ -0,0 +1,13 @@
+package com.ChatClone.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCloneBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatCloneBApplication.class, args);
+	}
+
+}
